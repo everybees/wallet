d9a1252415f6a6dfadc52807d0a7504f3ee4d311
everybees <everybees@gmail.com>
2022-01-21 14:01:37 +0100

    db

--- wallet/settings.py
+++ wallet/settings.py
@@ -174,5 +174,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'wallet/static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# prod_db = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
